b1b779880bdd14faad7e68000e2e2b46aaac3ead
Fausto Almeida <faustecn@hotmail.com>
2023-01-10 20:06:07 -0500

    Agregar archivos de proyecto.

---
+++ falmeidaExamen/falmeidaExamen/registro.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace falmeidaExamen
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class registro : ContentPage
+    {
+        public registro(string usuario)
+        {
+            InitializeComponent();
+            lblUsuario.Text = "Usuario Conectado: "+ usuario;
+        }
+
+       
+
+        private void btnCalCuotaMensual_Clicked(object sender, EventArgs e)
+        {
+            try 
+            {
+                double precio = Convert.ToDouble(txtMontoInicial.Text);
+
+                double valor = 4000 - precio;
+
+                double interes = 200;
+
+                double valorSobrante = valor;
+
+                double valorMensual = valor + interes;
+
+                double valorCuotas = valorMensual / 5;
+
+                if (precio < 4000)
+                {
+                    txtResCuotaMensual.Text = valorCuotas.ToString();
+                }
+                else {
+
+                    DisplayAlert("Alerta", "El precio excede el valor base.", "Cerrar");
+                }
+                
+            }
+            catch(Exception ex)
+            {
+                DisplayAlert("Alerta", ex.Message, "Cerrar");
+            }
+
+        }
+
+        private void bntCalcular_Clicked(object sender, EventArgs e)
+        {
+            string datos = lblUsuario.Text;
+            string nombreEstudiante = txtNombre.Text;
+
+            double precio = Convert.ToDouble(txtMontoInicial.Text);
+
+            double valor = 4000 - precio;
+
+            double interes = 200;
+
+            double valorSobrante = valor;
+
+            double valorMensual = valor + interes;
+
+            double valorCuotas = valorMensual / 5;
+
+            double montoCuotas = valorCuotas * 5;
+
+            double montoTotal = montoCuotas + precio;
+            Navigation.PushAsync(new resumen(datos, nombreEstudiante, valorCuotas, montoTotal));
+        }
+    }
+}
---
+++ falmeidaExamen/falmeidaExamen/resumen.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace falmeidaExamen
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class resumen : ContentPage
+    {
+        public resumen(string datos,string nombreEstudiante, double valorCuotas, double montoTotal)
+        {
+            InitializeComponent();
+            lblUsuario.Text = datos;
+            lblnombreEstudiante.Text = nombreEstudiante;
+            lblmontoTotal.Text = montoTotal.ToString();
+            lblmensuales.Text = valorCuotas.ToString();
+        }
+
+        private void btnInformacion_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Información","Este proyecto fue desarrollado por: Fausto Almeida de 9no A Sistemas de la Información.", "Cerrar");
+        }
+    }
+}
